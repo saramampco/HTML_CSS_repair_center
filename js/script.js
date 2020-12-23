3f3c7635091583bef5fced31aa31312fda2f6364
saramampco <avk0528@gmail.com>
2020-12-23 12:10:14 +0800

    1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 jQuery("#carousel").owlCarousel({
-   // autoplay: true,
+   autoplay: true,
    lazyLoad: true,
    rewind: true,
    margin: 20,
@@ -37,7 +37,7 @@ $(document).ready(function() {
       loop:true, //Зацикливаем слайдер
       margin:10, //Отступ от картино если выводите больше 1
       nav:false, //Отключил навигацию
-      // autoplay:true, //Автозапуск слайдера
+      autoplay:true, //Автозапуск слайдера
       smartSpeed:1000, //Время движения слайда
       autoplayTimeout:4000, //Время смены слайда
       responsive:{ //Адаптация в зависимости от разрешения экрана
